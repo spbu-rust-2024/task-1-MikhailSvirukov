3578f81524157e3c865f527615100a9eddcc17dc
Mikhail <mihailsvirukov@mail.ru>
2024-09-26 22:42:16 +0300

    minus 1...

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
 use std::io;
 
+
+
+
 fn main() {
     let mut array: Vec<i32>=Vec::new();
     let mut line=String::new();
     io::stdin()
         .read_line(&mut line)
         .expect("Failed to read line");
-    let chars : Vec<&str> = line.trim().split(' ').collect();
-    for i in 0..chars.len()-1 {
+    let chars: Vec<&str> = line.trim().split(' ').collect();
+
+    for i in 0..chars.len(){
         array.push(chars[i].parse::<i32>().unwrap());
     }
 
-let mut counter =1;
-while counter<array.len() {
-    let mut i = counter;
-    while i >0 {
-        if array[i] < array[i - 1] {
-            let t = array[i];
-            array[i] = array[i - 1];
-            array[i - 1] = t;
+    let mut counter =1;
+    while counter<array.len() {
+        let mut i = counter;
+        while i >0 {
+            if array[i] < array[i - 1] {
+                let t = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = t;
+            }
+            i -= 1;
         }
-        i -= 1;
+        counter+=1;
     }
-    counter+=1;
-}
 
 
     for elements in array{
-        print!("{} ", elements);
+        print!(" {}", elements);
     }
 
 }
