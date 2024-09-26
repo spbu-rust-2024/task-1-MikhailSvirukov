12e9daed1eadc8b196cc5eee30d2a8715d448bb4
Mikhail <mihailsvirukov@mail.ru>
2024-09-26 21:43:31 +0300

    done with algorithm, now input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+
 fn main() {
+    let mut array: Vec<i32>=Vec::new();
+    let mut line=String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
+    let chars : Vec<&str> = line.split(' ').collect();
+    for i in 0..chars.len()-1 {
+        array.push(chars[i].parse::<i32>().unwrap());
+    }
+
+let mut counter =1;
+while counter<array.len() {
+    let mut i = counter;
+    while i >0 {
+        if array[i] < array[i - 1] {
+            let t = array[i];
+            array[i] = array[i - 1];
+            array[i - 1] = t;
+        }
+        i -= 1;
+    }
+    counter+=1;
+}
+
+
+    print!("{:?}",array);
+
 
 }
